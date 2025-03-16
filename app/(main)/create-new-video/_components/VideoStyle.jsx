b33a4867676f55b3a0cfbb22a4bb80e7e2320b71
ashish-2106 <ashishjhajharia21@gmail.com>
2025-03-16 16:27:49 +0530

    modified video styles

--- app/(main)/create-new-video/_components/VideoStyle.jsx
+++ app/(main)/create-new-video/_components/VideoStyle.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const options = [
@@ -33,23 +33,33 @@ const options = [
     }
 ]
 function VideoStyle({ onHandelInputChange }) {
+    const [selectedStyle,setSelectedStyle] = useState();
     return (
         <div className='mt-5'>
             <h2>Video Style</h2>
             <p className='text-sm text-gray-400 mb-1'>Select Video Style</p>
 
-            <div className='grid grid-cols-2 lg:grid-col-3 xl:grid-cols-5 gap-2'>
-                {options?.map(((options, index) => (
-                    <div>
-                        <Image src={options.image} alt={options.name}
-                            width={500} height={120} className='object-cover h-[70px]
-                        lg:h-[90px] xl:h-[180px]'/>
-
+            <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2'>
+                {options?.map((option, index) => (
+                    <div key={option.id || index} className="relative"
+                    onClick={()=>{setSelectedStyle(option.name);
+                        onHandelInputChange('videoStyle',option.name)
+                    }}
+                    >
+                        <Image
+                            src={option.image}
+                            alt={option.name}
+                            width={500}
+                            height={120}
+                            className={`object-cover h-[90px] lg:h-[90px] xl:h-[180px]
+                             rounded-lg p-1 hover:border border-gray-300 cursor-pointer
+                             ${option.name == selectedStyle && 'border'} `}
+                        />
+                        <h2 className="absolute bottom-1 text-center w-full">{option.name}</h2>
                     </div>
-                )
-
                 ))}
             </div>
+
         </div>
     )
 }
